e59926ee83cf17fdbb7d8184d816ce113cd1937a
Abdukarim <abduhashimov04@gmail.com>
2023-04-02 23:17:52 +0300

    server-inst

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,25 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import path from "path";
+import {fileURLToPath} from "url";
 
 const app = express();
+
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: __dirname+'/.env' });
+mongoose.set('strictQuery', true);
+
+function handleError(e: any) {
+    console.log(`${e} didn't connect`)
+}
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO as string);
+        console.log("connected to mongoDB")
+    } catch (error) {
+        handleError(error);
+    }
+}
